53cd28a7e70d7cf8841ab7e20ad7f7ecac0db75b
vanderlei <vanderlei.loureiro@evoluum.com.br>
2023-11-28 18:31:09 -0300

    start project

---
+++ src/test/java/dev/vanderloureiro/springthreads/SpringThreadsApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.vanderloureiro.springthreads;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringThreadsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
